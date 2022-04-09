ff8fd90849cbc57a6efca3f04b3f76ced1a27f0b
Alkarim <alkarim@moralis.io>
2022-04-09 11:43:29 -0300

    Initial Project Commit
    MyRedCarpet Netflix Clone Initial Commit.

---
+++ Netflix-Starter/src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Home.css";
+import { Logo } from '../images/Netflix';
+import { ConnectButton, Icon } from 'web3uikit';
+import { TabList } from 'web3uikit';
+
+
+const Home = () => {
+
+return(
+  <>
+  <div className="logo">
+    <Logo />
+  </div>
+  <div className="connect">
+    <Icon fill='#ffffff' size={24} svg="bell" />
+    <ConnectButton />
+  </div>
+  <div className="topBanner">
+    <TabList defaultActiveKey={1} tabStyle="bar">
+      <Tab tabkey={1} tabName={"Movies"}>
+    </TabList>
+     
+  </div>
+  </>
+)
+}
+
+export default Home;
