7e3ddc3817c5df4d463ba23649ae1fb2da5fc1fb
Alkarim <alkarim@moralis.io>
2022-04-09 13:36:53 -0300

    Update Home.js

--- Netflix-Starter/src/pages/Home.js
+++ Netflix-Starter/src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import { Logo } from '../images/Netflix';
 import { ConnectButton, Icon, TabList, Tab } from 'web3uikit';
-import { Movies } from '../helpers/Library';
+import { Movies } from '../helpers/Library.js';
 
 const Home = () => {
 
@@ -19,6 +19,9 @@ return(
   <div className="topBanner">
     <TabList defaultActiveKey={1} tabStyle="bar">
       <Tab tabkey={1} tabName={"Movies"}></Tab>
+        <div className="scene">
+          <img src={movies[0].Scene} className="sceneimg"></img>
+        </div>
       <Tab tabkey={2} tabName={"Series"} isDisabled={true}></Tab>
       <Tab tabkey={3} tabName={"MyList"}></Tab>
     </TabList>
